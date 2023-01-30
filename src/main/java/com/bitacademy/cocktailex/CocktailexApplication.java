5dfde37b61a4923517797d3b924c651cfd4fbde1
dltjsam <dltjsam@gmail.com>
2023-01-30 19:10:21 +0900

    springboot #first init

---
+++ src/main/java/com/bitacademy/cocktailex/CocktailexApplication.java
@@ -0,0 +1,13 @@
+package com.bitacademy.cocktailex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocktailexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bitacademy/cocktailex/app/CocktailexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitacademy.cocktailex.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocktailexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
